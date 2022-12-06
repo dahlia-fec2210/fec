8dacf35e3e1d0a61f7651e15ef36d1327fadb6cc
Haillie W <hailliewilloughby@gmail.com>
2022-12-06 16:00:10 -0500

    Add product category prop and render it

--- src/components/overview/ProductInfo.jsx
+++ src/components/overview/ProductInfo.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StyleSelector from './StyleSelector.jsx';
 import AddToCart from './AddToCart.jsx';
 
-function ProductInfo({ productInfo }) {
+function ProductInfo({ productCategory }) {
+  // console.log('product info in ProductInfo:', productInfo);
+  const [productRating, setProductRating] = useState(''); // get from ???
+  // const [productCategory, setProductCategory] = useState('');
+  const [productName, setProductName] = useState('');
+  const [stylePrice, setStylePrice] = useState(''); // get from styles API
+
   return (
     <div>
       <h2>&gt; Product Info</h2>
       <h3>&gt; ⭐⭐⭐⭐⭐Product Rating</h3>
-      <h3>&gt; PRODUCT CATEGORY</h3>
+      <h3>{productCategory}</h3>
       <h3>&gt; Product Name</h3>
       <h3>&gt; $0.00</h3>
       <StyleSelector />
