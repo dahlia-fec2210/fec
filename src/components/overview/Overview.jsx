393d82f463dc5bb162597934f1c6d7aabe323c08
Haillie W <hailliewilloughby@gmail.com>
2022-12-06 11:20:04 -0500

    Import and render ProductInfo component

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Image from './Image.jsx';
+import ProductInfo from './ProductInfo.jsx';
 
 function Overview() {
   return (
     <div>
       <h1>Overview</h1>
       <Image />
+      <ProductInfo />
     </div>
   );
 }
