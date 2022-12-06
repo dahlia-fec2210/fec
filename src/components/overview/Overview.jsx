45a45f5053df642d44e73cbae9dd996f4576ffac
Haillie W <hailliewilloughby@gmail.com>
2022-12-06 12:54:14 -0500

    Add serverRoute prop to component and get request

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -3,8 +3,13 @@ import axios from 'axios';
 import Image from './Image.jsx';
 import ProductInfo from './ProductInfo.jsx';
 
-function Overview({ productId }) {
+function Overview({ productId, serverRoute }) {
   const [productInfo, setProductInfo] = useState(null);
+
+  axios.get(`${serverRoute}/products/${productId}`)
+    .then((info) => setProductInfo(info))
+    .then(() => console.log('product info in Overview:', productInfo));
+
   return (
     <div>
       <h1>Overview</h1>
