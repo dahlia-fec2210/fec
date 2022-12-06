e0414308e127ec9ef33f6e15f0259a259dcde74b
Haillie W <hailliewilloughby@gmail.com>
2022-12-06 11:16:44 -0500

    Import and render Image Set component

--- src/components/overview/Image.jsx
+++ src/components/overview/Image.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainImage from './MainImage.jsx';
+import ImageSet from './ImageSet.jsx';
 
 function Image() {
   return (
     <div>
       <h2>&gt; Image</h2>
       <MainImage />
+      <ImageSet />
     </div>
   );
 }
