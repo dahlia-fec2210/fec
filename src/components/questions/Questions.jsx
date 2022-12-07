c087b877286485c0bed4c561345f967aa8ef7b45
Aaron Mikulka <aaronjmikulka@gmail.com>
2022-12-07 13:39:59 -0700

    started refactoring

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -8,62 +8,44 @@ const serverRoute = `http://localhost:${process.env.PORT}`;
 
 function Questions({ currentProduct }) {
   const [allQuestions, setAllQuestions] = useState([]);
-  const [questions, setQuestions] = useState([]);
+  const [questionsList, setQuestionsList] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   // const [page, setPage] = useState(1);
-  const [countNum, setCountNum] = useState(0);
+  const [listCount, setListCount] = useState(null);
 
-  console.log(currentProduct, 'currentProduct');
-
-  let fetchQuestions = (allQuestionData, countInt) => {
-    // axios.get(`${serverRoute}/qa/questions`, {
-    //   params: {
-    //     product_id: productId,
-    //     page: pageNumber,
-    //     count: countNumber,
-    //   },
-    // });
-    let currentQuestions = [];
-    console.log(allQuestionData, countInt, 'fetchQuestions');
-    for (let i = 0; i < countInt; i++) {
-      currentQuestions.push(allQuestionData[i]);
-    }
-    console.log(currentQuestions, '010101010');
-    setQuestions(currentQuestions);
-  };
-    // .then((result) => {
-    //   console.log(result, '123123');
-    // });
-  const loadQuestions = () => {
-    console.log(allQuestions, 'loadQuestions');
-    setCountNum(countNum + 2);
-    fetchQuestions(allQuestions, countNum);
-    // const newPage = page + 1;
-    // setPage(newPage);
-    // setCount(count + 2);
-    // fetchQuestions(currentProduct, newPage, 2)
-    //   .then((results) => {
-    //     console.log(results.data.results, 'testing in line 32');
-    //     setQuestions([...questions, ...results.data.results]);
-    //   });
-  };
-
-  useEffect(() => {
-    // if (currentProduct !== null) {
-    console.log('first useeffect');
-    axios.get(`${serverRoute}/qa/questions`, {
-      params: {
-        product_id: currentProduct,
-      },
-    })
-      .then((response) => {
-        const questionsArr = response.data.results;
-        console.log(response, 'in response');
-        setAllQuestions(questionsArr);
-        setIsLoading(false);
-      });
-    // }
-  }, []);
+  // let fetchQuestions = (allQuestionData, countInt) => {
+  //   // axios.get(`${serverRoute}/qa/questions`, {
+  //   //   params: {
+  //   //     product_id: productId,
+  //   //     page: pageNumber,
+  //   //     count: countNumber,
+  //   //   },
+  //   // });
+  //   let currentQuestions = [];
+  //   console.log(allQuestionData, countInt, 'fetchQuestions');
+  //   for (let i = 0; i < countInt; i++) {
+  //     currentQuestions.push(allQuestionData[i]);
+  //   }
+  //   console.log(currentQuestions, '010101010');
+  //   setQuestions(currentQuestions);
+  // };
+  //   // .then((result) => {
+  //   //   console.log(result, '123123');
+  //   // });
+  // const loadQuestions = (newAllQuestions) => {
+  //   console.log(allQuestions, 'loadQuestions');
+  //   const newCountNum = countNum + 2;
+  //   setCountNum(newCountNum);
+  //   fetchQuestions(newAllQuestions, newCountNum);
+  //   // const newPage = page + 1;
+  //   // setPage(newPage);
+  //   // setCount(count + 2);
+  //   // fetchQuestions(currentProduct, newPage, 2)
+  //   //   .then((results) => {
+  //   //     console.log(results.data.results, 'testing in line 32');
+  //   //     setQuestions([...questions, ...results.data.results]);
+  //   //   });
+  // };
 
   let compareFn = (a, b) => {
     if (a.question_helpfulness > b.question_helpfulness) {
@@ -74,28 +56,50 @@ function Questions({ currentProduct }) {
     return 0;
   };
 
-  useEffect(() => {
-    allQuestions.sort(compareFn);
-    loadQuestions(allQuestions);
-    // setQuestions(allQuestions);
-    console.log(allQuestions, 'second useEffect');
-  }, [allQuestions]);
+  const fetchAllQuestions = () => axios.get(`${serverRoute}/qa/questions`, {
+    params: {
+      product_id: currentProduct,
+    },
+  })
+    .then((response) => {
+      const questionsArr = [...response.data.results].sort(compareFn);
+      setAllQuestions(questionsArr);
+      setIsLoading(false);
+      return questionsArr;
+    });
 
-  let content;
-  if (isLoading) {
-    content = <div className="QuestionLoading">Loading...</div>;
-  } else {
-    content = (
-      questions.map((individualQ, index) => <QuestionEntry key={index} question={individualQ} />)
-    );
-  }
+  useEffect(() => {
+    fetchAllQuestions()
+      .then((questionsArr) => {
+        setQuestionsList(questionsArr.slice(0, 2));
+        setListCount(2);
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   const newAllQuestions = [...allQuestions];
+  //   newAllQuestions.sort(compareFn);
+  //   setAllQuestions(newAllQuestions);
+  //   loadQuestions(newAllQuestions);
+  //   // setQuestions(allQuestions);
+  //   console.log(allQuestions, 'second useEffect');
+  // }, [allQuestions]);
+
+  // let content;
+  // if (isLoading) {
+  //   content = <div className="QuestionLoading">Loading...</div>;
+  // } else {
+  //   content = (
+  //     questionsList.map((individualQ, index) => <QuestionEntry key={index} question={individualQ} />)
+  //   );
+  // }
 
   return (
     <div>
       <h1>Questions</h1>
-      {content}
-      {countNum > allQuestions.length ? null
-        : <LoadQuestionButton handleClick={loadQuestions} />}
+      {questionsList && questionsList.map((individualQ, index) => <QuestionEntry key={index} question={individualQ} />)}
+      {listCount > allQuestions.length ? null
+        : <LoadQuestionButton /* handleClick={loadQuestions} */ />}
     </div>
   );
 }
