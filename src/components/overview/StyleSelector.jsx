6fe5a85ec904fcdca4e20c2e24bcd1791f08422c
Haillie W <hailliewilloughby@gmail.com>
2022-12-07 14:31:42 -0500

    Return Styles comp passing each style from prop

--- src/components/overview/StyleSelector.jsx
+++ src/components/overview/StyleSelector.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Styles from './Styles.jsx';
 
-function StyleSelector() {
+function StyleSelector({ productStyles }) {
   return (
     <div>
       <h3>&gt; Style Selector comp</h3>
       <h4>&gt; STYLE &gt; SELECTED STYLE</h4>
-      <Styles />
+      {productStyles.map((style, i) => (
+        <Styles key={i} style={style} />
+      ))}
     </div>
   );
 }
