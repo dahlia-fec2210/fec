109a403911261817942582cbdd374a666f809838
Haillie W <hailliewilloughby@gmail.com>
2022-12-07 12:20:56 -0500

    Add/render productRating prop from Overview

--- src/components/overview/ProductInfo.jsx
+++ src/components/overview/ProductInfo.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import StyleSelector from './StyleSelector.jsx';
 import AddToCart from './AddToCart.jsx';
 
-function ProductInfo({ productCategory, productName }) {
-  const [productRating, setProductRating] = useState(''); // get from ???
+function ProductInfo({ productCategory, productName, productRating }) {
+  // const [productRating, setProductRating] = useState(''); // get from ???
   // const [productName, setProductName] = useState('');
   const [stylePrice, setStylePrice] = useState(''); // get from styles API
 
   return (
     <div>
       <h2>&gt; Product Info</h2>
-      <h3>&gt; ⭐⭐⭐⭐⭐Product Rating</h3>
+      <h3>{productRating}</h3>
       <h3>{productCategory}</h3>
       <h3>{productName}</h3>
       <h3>&gt; $0.00</h3>
