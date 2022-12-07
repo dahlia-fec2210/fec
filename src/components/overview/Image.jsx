bd78ea047b93884840b3329b8cd6c966d8ff1bf7
Haillie W <hailliewilloughby@gmail.com>
2022-12-07 14:58:09 -0500

    Add style photos prop/render ImageSet w/ each

--- src/components/overview/Image.jsx
+++ src/components/overview/Image.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import MainImage from './MainImage.jsx';
 import ImageSet from './ImageSet.jsx';
 
-function Image({ image }) {
+function Image({ image, currentStylePhotos }) {
+  console.log('current style in Image comp:', currentStylePhotos);
+  // console.log('current style photos:', currentStyle.photos);
   return (
     <div>
       <MainImage mainImage={image} />
-      <ImageSet />
+      {currentStylePhotos.map((photo, i) => (
+        <ImageSet key={i} photo={photo} />
+      ))}
     </div>
   );
 }
