0cb9f3bae2f0fe37f17dd1eacec72e7a1feaa929
Haillie W <hailliewilloughby@gmail.com>
2022-12-08 10:21:23 -0500

    Change element to render style thumbnails

--- src/components/overview/Styles.jsx
+++ src/components/overview/Styles.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 function Styles({ style }) {
+  // console.log('style.photos[0] in Style component:', style.photos[0]);
   return (
-    <h3>{style.name}</h3>
+    <img src={style.photos[0].thumbnail_url} alt="" />
   );
 }
 
