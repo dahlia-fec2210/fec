62371d35a368b5928c5ef1a1c94394795dd0e19e
Haillie W <hailliewilloughby@gmail.com>
2022-12-08 15:09:54 -0500

    Wrap div around img element

--- src/components/overview/Styles.jsx
+++ src/components/overview/Styles.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Styles({ style }) {
   return (
-    <img className="styles" src={style.photos[0].thumbnail_url} alt="" />
+    <div>
+      <img className="styles" src={style.photos[0].thumbnail_url} alt="" />
+    </div>
   );
 }
 
