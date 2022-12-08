53a9300c6d5233d9aac0af0220a45cd1535b1908
Haillie W <hailliewilloughby@gmail.com>
2022-12-08 16:35:38 -0500

    Implement main image carousel

--- src/components/overview/Image.jsx
+++ src/components/overview/Image.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainImage from './MainImage.jsx';
 import ImageSet from './ImageSet.jsx';
 
 function Image({ image, currentStylePhotos }) {
-  console.log('current style in Image comp:', currentStylePhotos);
-  // console.log('current style photos:', currentStyle.photos);
+  // console.log('current style photos in Image comp:', currentStylePhotos);
+
+  const [currentMainImage, setCurrentMainImage] = useState(0);
+  const { length } = currentStylePhotos;
+
+  const prevImage = () => {
+    setCurrentMainImage(currentMainImage === 0 ? length - 1 : currentMainImage - 1);
+  };
+
+  const nextImage = () => {
+    setCurrentMainImage(currentMainImage === length - 1 ? 0 : currentMainImage + 1);
+  };
+
+  console.log('current main image index:', currentMainImage);
+
   return (
     <div>
-      <MainImage mainImage={image} />
+      <div className="main-carousel">
+        <i className="left-arrow fa-solid fa-chevron-left fa-2xl" onClick={prevImage} />
+        <i className="right-arrow fa-solid fa-chevron-right fa-2xl" onClick={nextImage} />
+
+        {currentStylePhotos.map((photo, i) => (
+          <div className={i === currentMainImage ? 'slide-active' : 'slide'} key={i}>
+            {i === currentMainImage && <MainImage key={i} mainImage={image} photo={photo} />}
+          </div>
+        ))}
+      </div>
+
       {currentStylePhotos.map((photo, i) => (
         <ImageSet key={i} photo={photo} />
       ))}
