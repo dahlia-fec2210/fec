267bbdede199641e79a156fa3b0734a6ac00baa3
Haillie W <hailliewilloughby@gmail.com>
2022-12-09 17:49:47 -0500

    Add onclick handler to style img

--- src/components/overview/Styles.jsx
+++ src/components/overview/Styles.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function Styles({ style }) {
+  const handleStyleClick = () => {
+    //
+  };
+
   return (
     <div>
-      <img className="styles" src={style.photos[0].thumbnail_url} alt="" />
+      <img className="styles" src={style.photos[0].thumbnail_url} alt="" onClick={handleStyleClick} />
     </div>
   );
 }
