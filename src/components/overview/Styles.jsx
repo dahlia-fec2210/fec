e5f4956a5c7a8a1ea0cd7e5038b4e4ffb298ecce
Haillie W <hailliewilloughby@gmail.com>
2022-12-09 18:49:40 -0500

    Clean up code

--- src/components/overview/Styles.jsx
+++ src/components/overview/Styles.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Styles({ style }) {
+function Styles({ style, setCurrentStylePhotos }) {
   const handleStyleClick = () => {
-    //
+    setCurrentStylePhotos(style.photos);
   };
 
   return (
