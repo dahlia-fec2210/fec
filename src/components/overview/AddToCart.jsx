599dd253fd5b62463c11bf6217d0f41c22800f45
Haillie W <hailliewilloughby@gmail.com>
2022-12-09 06:19:09 -0500

    Disable size selector if there is no stock

--- src/components/overview/AddToCart.jsx
+++ src/components/overview/AddToCart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SizeOptions from './SizeOptions.jsx';
 
 function AddToCart({ currentStyleSkus }) {
@@ -9,15 +9,23 @@ function AddToCart({ currentStyleSkus }) {
 
   return (
     <form onSubmit={handleSubmit}>
+      {Object.keys(currentStyleSkus).length === 0 && (
+      <select disabled>
+        <option value="">OUT OF STOCK</option>
+      </select>
+      )}
+
       <select>
         <option value="">SELECT SIZE</option>
         {Object.values(currentStyleSkus).map((sku, i) => (
           <SizeOptions key={i} size={sku.size} />
         ))}
       </select>
+
       <select>
         <option value="">1</option>
       </select>
+
       <button type="submit">Add to Cart/Bag</button>
     </form>
   );
