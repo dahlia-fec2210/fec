51f6674b33c0edc5142641e81e1b893876a69310
Haillie W <hailliewilloughby@gmail.com>
2022-12-09 21:43:15 -0500

    Add state/func for selected style/pass to Styles

--- src/components/overview/StyleSelector.jsx
+++ src/components/overview/StyleSelector.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './Styles.jsx';
 
 function StyleSelector({ productStyles, setCurrentStylePhotos }) {
+  const [selected, setSelected] = useState(0);
+
+  const handleStyleClick = (index, style) => {
+    setSelected(index);
+    setCurrentStylePhotos(style.photos);
+  };
+
   return (
     <div>
       <h4>STYLE &gt; SELECTED STYLE</h4>
       <div className="styles-selector">
-        {productStyles.map((style) => {
+        {productStyles.map((style, i) => {
           console.log('style in StylesSelector:', style);
           return (
             <Styles
               key={style.style_id}
+              index={i}
               style={style}
               setCurrentStylePhotos={setCurrentStylePhotos}
+              selected={selected}
+              handleStyleClick={handleStyleClick}
             />
           );
         })}
