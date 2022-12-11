9b024760d340328e17006bfa64722da0a149906f
Haillie W <hailliewilloughby@gmail.com>
2022-12-11 13:00:29 -0500

    Refactor returned elements to show fixed # of imgs

--- src/components/overview/ImageSet.jsx
+++ src/components/overview/ImageSet.jsx
@@ -1,24 +1,68 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable no-shadow */
+/* eslint-disable react/no-array-index-key */
 import React from 'react';
 
-function ImageSet({
-  photo, index, selected, handleThumbnailClick,
-}) {
-  let indicator;
-  if (index === selected) {
-    indicator = <div className="thumbnail-overlay" />;
-  } else {
-    indicator = null;
-  }
+function ImageSet({ imageSetCarousel, handleThumbnailClick, currentStylePhotos }) {
+  // let indicator;
+  // if (index === selected) {
+  //   indicator = <div className="thumbnail-overlay" />;
+  // } else {
+  //   indicator = null;
+  // }
 
   return (
     <div className="image-thumbnails">
-      <img
-        className="thumbnails"
-        src={photo.thumbnail_url}
-        alt=""
-        onClick={() => handleThumbnailClick(index)}
-      />
-      {/* {indicator} */}
+      <div className="thumbnails-container">
+        <img
+          className="thumbnails"
+          src={currentStylePhotos[imageSetCarousel[0]].thumbnail_url}
+          alt=""
+          onClick={() => handleThumbnailClick(imageSetCarousel[0])}
+        />
+      </div>
+      <div className="thumbnails-container">
+        <img
+          className="thumbnails"
+          src={currentStylePhotos[imageSetCarousel[1]].thumbnail_url}
+          alt=""
+          onClick={() => handleThumbnailClick(imageSetCarousel[1])}
+        />
+      </div>
+      <div className="thumbnails-container">
+        <img
+          className="thumbnails"
+          src={currentStylePhotos[imageSetCarousel[2]].thumbnail_url}
+          alt=""
+          onClick={() => handleThumbnailClick(imageSetCarousel[2])}
+        />
+      </div>
+      <div className="thumbnails-container">
+        <img
+          className="thumbnails"
+          src={currentStylePhotos[imageSetCarousel[3]].thumbnail_url}
+          alt=""
+          onClick={() => handleThumbnailClick(imageSetCarousel[3])}
+        />
+      </div>
+      <div className="thumbnails-container">
+        <img
+          className="thumbnails"
+          src={currentStylePhotos[imageSetCarousel[4]].thumbnail_url}
+          alt=""
+          onClick={() => handleThumbnailClick(imageSetCarousel[4])}
+        />
+      </div>
+      <div className="thumbnails-container">
+        <img
+          className="thumbnails"
+          src={currentStylePhotos[imageSetCarousel[5]].thumbnail_url}
+          alt=""
+          onClick={() => handleThumbnailClick(imageSetCarousel[5])}
+        />
+      </div>
     </div>
   );
 }
