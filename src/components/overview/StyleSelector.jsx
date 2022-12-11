b110908c76982d5cb78332b456c0e777738f6e70
Haillie W <hailliewilloughby@gmail.com>
2022-12-11 13:15:25 -0500

    Add state to render current style name

--- src/components/overview/StyleSelector.jsx
+++ src/components/overview/StyleSelector.jsx
@@ -3,18 +3,25 @@ import Styles from './Styles.jsx';
 
 function StyleSelector({ productStyles, setCurrentStylePhotos }) {
   const [selected, setSelected] = useState(0);
+  const [styleName, setStyleName] = useState(productStyles[0].name);
 
   const handleStyleClick = (index, style) => {
     setSelected(index);
     setCurrentStylePhotos(style.photos);
+    setStyleName(style.name);
   };
 
   return (
     <div>
-      <h4>STYLE &gt; SELECTED STYLE</h4>
+      <h4>
+        STYLE &gt;
+        {' '}
+        {styleName}
+      </h4>
       <div className="styles-selector">
         {productStyles.map((style, i) =>
           // console.log('style in StylesSelector:', style);
+          // eslint-disable-next-line implicit-arrow-linebreak
           (
             <Styles
               key={style.style_id}
