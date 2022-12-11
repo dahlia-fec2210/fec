bd3f0d566c299e584fcf54015eeddd9f5cfa6882
Haillie W <hailliewilloughby@gmail.com>
2022-12-11 13:19:53 -0500

    Add linter rules

--- src/components/overview/Styles.jsx
+++ src/components/overview/Styles.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 
 function Styles({
