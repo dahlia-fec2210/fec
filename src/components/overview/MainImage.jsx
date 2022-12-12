95835723474a858b8d99726b405cfd440ecaeb15
Haillie W <hailliewilloughby@gmail.com>
2022-12-12 13:08:05 -0500

    Change class conditions and add props for zoomed

--- src/components/overview/MainImage.jsx
+++ src/components/overview/MainImage.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 
-function MainImage({ photo, expanded, setExpanded }) {
+function MainImage({ photo, expanded, setExpanded . zoomed, setZoomed}) {
   const handleMainImageClick = () => {
     setExpanded(true);
   };
+
+  let containerClass;
+  if (expanded === false) {
+    containerClass = 'default-image';
+  } else if (expanded === true) {
+    containerClass = 'expanded-image';
+  } else if (zoomed === true) {
+    containerClass = 'zoomed-image';
+  }
+
+  let imageClass;
+  if (expanded === false) {
+    imageClass = 'main-image';
+  } else if (expanded === true) {
+    imageClass = 'expanded-main-image';
+  } else if (zoomed === true) {
+    imageClass = 'zoomed-main-image';
+  }
+
   return (
-    <div className={expanded === false ? 'default-image' : 'expanded-image'} onClick={handleMainImageClick}>
-      <img className="main-image" src={photo.url} alt="" />
+    <div className={containerClass} onClick={handleMainImageClick}>
+      <img className={expanded === false ? 'main-image' : 'expanded-main-image'} src={photo.url} alt="" />
     </div>
   );
 }
