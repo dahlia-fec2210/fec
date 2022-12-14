469346d6e09c51f48753df3bbed516c2510dd6f4
Haillie W <hailliewilloughby@gmail.com>
2022-12-14 12:58:19 -0500

    Merge

--- tests/overview.test.js
+++ tests/overview.test.js
@@ -6,30 +6,6 @@ import ProductInfo from '../src/components/overview/ProductInfo.jsx';
 import Overview from '../src/components/overview/Overview.jsx';
 import StyleSelector from '../src/components/overview/StyleSelector.jsx';
 
-// const style = [{ name: '***BLACK STYLE***', style_id: 2, photos: { thumbnail_url: '' } }];
-
-// describe('Style Selector test', () => {
-//   const user = userEvent.setup();
-//   it('should render the style name', () => {
-//     render(<StyleSelector productStyles={style} />);
-//     screen.debugger;
-//     // expect(screen.getByTestId('category')).toHaveTextContent('JACKETS');
-//   });
-// });
-
-// const category = {
-//   productCategory: 'JACKETS',
-// };
-
-// describe('Product Info test', () => {
-//   const user = userEvent.setup();
-//   it('should render the default product\'s category', () => {
-//     render(<ProductInfo />);
-//     screen.debugger;
-//     expect(screen.getByTestId('category')).toHaveTextContent('JACKETS');
-//   });
-// });
-
 describe('AddToCart test', () => {
   const user = userEvent.setup();
   it('should render the current style skus', () => {
@@ -46,19 +22,3 @@ const currentStyleSkus = {
   1281035: { quantity: 4, size: 'XL' },
   1281036: { quantity: 4, size: 'XL' },
 };
-
-// describe('Overview test', () => {
-//   const user = userEvent.setup();
-//   it('should render the Overview component', () => {
-//     render(<Overview productId={37311} />);
-//     screen.debugger;
-//     // expect(screen.getByTestId('category')).toHaveTextContent('JACKETS');
-//   });
-// });
-
-/// /////////////////////// MINE ////////////////////////////
-
-// test('renders product category', async () => {
-//   render(<ProductInfo productCategory={productCategory} />);
-//   expect(screen.getByTestId('category')).toHaveTextContent('JACKETS');
-// });
