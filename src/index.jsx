a404d080d9b46920007eb0c5bffbebe59df8a6ea
Aaron Mikulka <aaronjmikulka@gmail.com>
2022-12-15 09:43:50 -0700

    fixed small bug that came with merge

--- src/index.jsx
+++ src/index.jsx
@@ -81,7 +81,6 @@ function App() {
             />
           ) }
         { currentProduct === null ? <div>Loading...</div>
-        { currentProduct === null ? <div>Loading...</div>
           : <Questions currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} /> }
         { currentProduct === null ? <div>Loading...</div>
           : <Reviews currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} reviewsRef={reviewsRef} /> }
